dce6db6458490d7c6e9e531e246830ad2bfaf892
Ozkan Ardil <ozkanardil@gmail.com>
2021-03-14 13:06:53 +0300

    Initial Commit

---
+++ AppSettingsApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using AppSettingsApp.Entity;
+using AppSettingsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSettingsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string AppName = AppSettings.AppName;
+            vmHome data = new vmHome();
+            data.HomeAppName = AppName;
+            return View(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AppSettingsApp/Models/vmHome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSettingsApp.Models
+{
+    public class vmHome
+    {
+        public string HomeAppName { get; set; }
+    }
+}
